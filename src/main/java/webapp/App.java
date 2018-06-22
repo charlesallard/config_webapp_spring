5cc3251a61f16079fbfe6be17480ba119d4d4556
charles <charlesal@hotmail.fr>
2018-06-22 16:20:58 +0200

    added get function to controller

--- src/main/java/webapp/App.java
+++ src/main/java/webapp/App.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("fr.iocean")
+@ComponentScan("webapp")
 public class App {
 
 }
--- src/main/java/webapp/MyController.java
+++ src/main/java/webapp/MyController.java
@@ -1,15 +1,43 @@
 package webapp;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/api/myFirstService")
+@RequestMapping("")
 public class MyController {
-   
-  @RequestMapping(value = "/hello", method = RequestMethod.GET)
-   public String foo() {
-       return "Hello world";
-   }
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<String> foo2() {
+        return new ResponseEntity<String>("{}", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    public ResponseEntity<String> foo() {
+        return new ResponseEntity<String>("Appli de mes ghjghujrftyèr", HttpStatus.OK);
+    }
+    
+    @GetMapping(value = "/images/search/{annee}/{month}/{day}")
+    public ResponseEntity<String> affiche(@PathVariable("annee") String annee, @PathVariable("month") String month, @PathVariable("day") String day, @RequestParam("sort") String sort){
+    	//return new ResponseEntity<String>( , HttpStatus.OK);
+    	LocalDate loc = LocalDate.of(Integer.parseInt(annee), Integer.parseInt(month), Integer.parseInt(day));
+    	return new ResponseEntity<String>("Liste des images du " + loc.format(DateTimeFormatter.ofPattern("dd MMMM yyyy")) + " trié par " + sort, HttpStatus.OK);
+    }
+    
+    
+    @GetMapping
+    public String setupForm(@PathVariable("petId") int petId) {
+		return null;    	
+    }
 }
